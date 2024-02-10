3331ed47a4ff68a10b143d35a325769740e65c61
FrostSalamander <108145197+FrostSalamander@users.noreply.github.com>
2024-02-10 22:32:19 +0000

    fix: rename zip file

--- zip.sh
+++ zip.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-zip -r tdm-latch.pk4 \
+zip -r tdm-latch \
   * \
   -x "maps/*" -x "dds/textures/*" -x "textures/*" darkmod.txt startingmap.txt zip.sh materials/welliadd_proto.mtr
